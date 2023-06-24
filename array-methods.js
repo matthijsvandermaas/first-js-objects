c701cad92f50fe1b63853311a33520194f97cad2
matthijsvandermaas <matthijsvandermaas@yahoo.com>
2023-06-24 16:27:42 +0200

    Beschrijving van de wijzigingen

--- array-methods.js
+++ array-methods.js
@@ -29,12 +29,25 @@ console.log(pizzas)
 
 const brownieIngredients = ["Noten", "Eieren", "Bloem", "Cacao", "Chocoladestukjes", "Bakpoeder"];
 
+console.log(brownieIngredients);
+
+// Verwijder het eerste item uit de array
+brownieIngredients.splice(0, 1);
+console.log(brownieIngredients);
 
 /* Opdracht 3: */
 // Draai de volgorde van onderstaande array met namen om en log de gehele array in de terminal om te kijken is het gelukt is
 // ---- Verwachte uitkomst: [ 'Piet', 'Jan', 'Henk' ]
 
-const names = ["Henk", "Jan", "Piet"];
+const names = ['Henk', 'Jan', 'Piet'];
+const reversedNames = names.reverse();
+
+console.log(reversedNames);
+
+names.splice(1, 1); // Verwijder het tweede element ('Jan')
+names.unshift('Jan'); // Voeg 'Jan' en 'Piet' toe aan het begin van de array
+console.log(names);
+
 
 
 
@@ -43,6 +56,12 @@ const names = ["Henk", "Jan", "Piet"];
 // ---- Verwachte uitkomst: [ 'Utrecht', 'Amsterdam', 'Maastricht', 'Eindhoven' ]
 
 const cities = ["Utrecht", "Maastricht", "Eindhoven"];
+
+const plaatsnamen = ['Utrecht', 'Maastricht', 'Eindhoven'];
+const newPlaats ='Amsterdam'
+plaatsnamen.splice(1, 0, newPlaats); // Voeg 'Amsterdam' toe op index 1
+console.log(plaatsnamen);
+
 
 
 
